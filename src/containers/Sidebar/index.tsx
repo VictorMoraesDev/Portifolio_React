bd5292f0cd3513f8c0832585079a308cd68a319e
Victor Moraes <victorf.moraes@hotmail.com>
2024-06-06 19:08:06 -0300

    estilizando o sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+
+import { Descricao, Botaotema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/VictorMoraesDev.png" alt="Foto perfil" />
-    <Titulo fontSize={20}>Victor Moraes</Titulo>
+    <SidebarContainer>
+    <Avatar />
+    <Titulo fontSize={18}>Victor Moraes</Titulo>
+    <Paragrafo tipo="secundario" fontSize={16}>
+      VictorMoraesDev
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <Botaotema>Trocar tema</Botaotema>
+    </SidebarContainer>
   </aside>
 )
 
